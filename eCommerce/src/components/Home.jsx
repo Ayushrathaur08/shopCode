2b3069425d9a33057208f8694dda41b31e9e07ee
Ayush Rathaur <103485950+Ayushrathaur08@users.noreply.github.com>
2025-07-03 23:05:08 +0530

    Add image carousel and enhance footer layout
    Integrated Swiper.js for an image carousel on the Home page and added seven new images. Enhanced the Layout component with a new multi-column footer featuring website links, social media, brand info, and a contact form. Updated dependencies to include Swiper.

--- eCommerce/src/components/Home.jsx
+++ eCommerce/src/components/Home.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import Layout from './Layout'
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+
 
 const Home = () => {
   return (
     <Layout>
-      <div>Home</div>
+      <Swiper
+        style={{ height: "400px", width: "100%" }}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        navigation
+      >
+        <SwiperSlide>
+          <img src="/images/pic1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/pic2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/pic3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/pic4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/pic5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/pic6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/pic7.jpg" />
+        </SwiperSlide>
+      </Swiper>
     </Layout>
   );
 }
--- eCommerce/src/components/Layout.jsx
+++ eCommerce/src/components/Layout.jsx
@@ -44,12 +44,89 @@ const Layout = ({ children }) => {
               </Link>
             </ul>
           </div>
-            </nav>
-            
+        </nav>
 
-        <footer className='bg-orange-600 p-16'>
-            <div className='w-10/12 mx-auto'></div>
-                
+        <div>{children}</div>
+
+        <footer className="bg-orange-600 py-16 mb-0">
+          <div className="w-10/12 mx-auto grid grid-cols-4">
+            <div className="space-y-2">
+              <h1 className="text-white font-semibold text-2xl">
+                Website Links
+              </h1>
+              <ul className="space-y-2 text-white">
+                {menus.map((item, index) => (
+                  <div key={index}>
+                    <Link to={item.href}>{item.label}</Link>
+                  </div>
+                ))}
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/signup">SignUp</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="space-y-2">
+              <h1 className="text-white font-semibold text-2xl">Follow Us</h1>
+              <ul className="space-y-2 text-white">
+                <li>
+                  <Link to="/">FaceBook</Link>
+                </li>
+                <li>
+                  <Link to="/">Instagram</Link>
+                </li>
+                <li>
+                  <Link to="/">Youtube</Link>
+                </li>
+                <li>
+                  <Link to="/">Linkdln</Link>
+                </li>
+                <li>
+                  <Link to="/">X</Link>
+                </li>
+              </ul>
+            </div>
+            <div className='pr-8'>
+              <h1 className="text-white font-semibold text-2xl">Brand Deals</h1>
+              <p className="text-gray-100 mb-6">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+                aut eum voluptate, asperiores culpa molestias cum amet natus.
+              </p>
+              <img src="/images/logo3.png" className="w-[120px]" />
+            </div>
+            <div>
+              <h1 className="text-white font-semibold text-2xl">Contact Us</h1>
+              <form action="" className="space-y-2">
+                <input
+                  required
+                  name="name"
+                  placeholder="Your Name"
+                  className="bg-white w-full rounded p-2"
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Your Email"
+                  className="bg-white w-full rounded p-2"
+                />
+                <textarea
+                  placeholder="Enter ur message"
+                  className="bg-white w-full rounded p-2"
+                  rows={3}
+                  required
+                  name="message"
+                />
+                <button
+                  type="submit"
+                  className="bg-black text-white py-3 cursor-pointer w-full rounded text-xl"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
         </footer>
       </div>
     );
