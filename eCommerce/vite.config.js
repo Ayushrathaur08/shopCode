0440d2c0dd2af18ae5eb0dd6c21bd1e3dade98b7
Ayush Rathaur <103485950+Ayushrathaur08@users.noreply.github.com>
2025-07-08 03:57:23 +0530

    Update vite.config.js

--- eCommerce/vite.config.js
+++ eCommerce/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()
-  ,]
-})
+  base: "/shopCode/", // 👈 IMPORTANT: your repo name
+  plugins: [react(), tailwindcss()],
+});
